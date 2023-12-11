69c0d84ec9c41ffbe795f2471203b4a6fb508b72
SalamiTech <salamigideon01@gmail.com>
2023-12-11 22:37:02 +0000

    .

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,10 @@
+import 'package:news_paper_app/core/resources/data_state.dart';
+import 'package:news_paper_app/features/daily_news/data/models/article.dart';
+import 'package:news_paper_app/features/daily_news/domain%20/entities/articles.dart';
+import 'package:news_paper_app/features/daily_news/domain%20/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  Future<DataState<List<ArticleModel>>> getNewsArticles() {
+    throw UnimplementedError();
+  }
+}
